b258ede1edc64f455233ab46ac19b55b037b6328
xyz0703 <44663301+xyz0703@users.noreply.github.com>
2019-01-07 10:50:43 +0800

    Update js1.js

--- js1/js1.js
+++ js1/js1.js
@@ -1,58 +1,43 @@
-    var div=document.getElementsByTagName('div');
-var colors=['red','plum','blue','green','yellow','cyan','black','pink','gray','brown'];
-btnone.onclick=function() {//点击开始
-    c=setInterval(function() {//使用定时器
-        start();//调用函数
+var  div = document.getElementsByClassName('square');
+var btnone = document.getElementById('btnone');
+var btntwo = document.getElementById('btntwo');
+var time;
+function begin() {
+    var one = Math.floor(Math.random()*div.length);
+    var two = Math.floor(Math.random()*div.length);
+    var three = Math.floor(Math.random()*div.length);
+    if(one == two){
+        one = Math.floor(Math.random()*div.length);
+    }else if(two == three){
+        two = Math.floor(Math.random()*div.length);
+    }else if(one = three){
+        three = Math.floor(Math.random()*div.length);
     }
-    ,1000);//设置时间
+    div[one].style.backgroundColor = 'rgb'+ colors();
+    div[two].style.backgroundColor = 'rgb'+ colors();
+    div[three].style.backgroundColor = 'rgb'+ colors();
 }
-    btntwo.onclick=function() {//停止按钮的
-        for(i=0;i<div.length;i++) {//循环
-            div[i].style.background="#FFA600";//遍历清除颜色
+function colors() {
+    var rgb;
+    var r = Math.floor(Math.random()*265);
+    var g = Math.floor(Math.random()*265);
+    var b = Math.floor(Math.random()*265);
+    rgb = '('+r+','+g+','+b+')';
+    return rgb;
+}
+btnone.onclick = function(){
+    clearInterval(time);
+    time = setInterval(function(){
+        for(var i = 0; i <  div.length; i++){
+            div[i].style.backgroundColor = '';
         }
-        clearInterval(c);//停止定时器
-    }
-    function start() {
-        for(var i=0;i<div.length;i++) {//每次随机颜色时遍历将背景设置好
-            div[i].style.background="#FFA600";
-        }
-        var arr=new Array(3);//创建数组容纳随机数
-        var arr1=new Array(3);
-        for(var i=0;i<arr.length;i++) {//创建第一组数组
-            var a=parseInt(Math.random()*9); 
-            console.log(a);
-            if(i==0) {//第一个数字直接导入数组
-                arr[i]=a;
-            }
-            else{
-                for(var j=0;j<i;j++) {//第二个与第三个数字进行判断
-                if(a==arr[j]) {//如果重复从新开始
-                    i--
-                }
-            else {
-                arr[i]=a;
-                }
-            }
-        }
-    }
-    for(var i=0;i<arr1.length;i++) {//同上。随机颜色
-        var a=parseInt(Math.random()*9); 
-        if(i==0) {
-           arr1[i]=a;
-        }
-        else {
-            for(var j=0;j<i;j++) {
-                if(a==arr1[j]) {
-                    i--
-                }
-                else {
-                    arr1[i]=a;
-                }
-            }
-        }
-    }
-    for(var i=0;i<arr.length;i++) {
-        div[arr[i]].style.background=colors[arr1[i]];//将随机的颜色给随机的地址
+        begin();
+
+    },1000);
+}
+btntwo.onclick = function(){
+    clearInterval(time);
+    for(var i = 0; i <  div.length; i++){
+        div[i].style.backgroundColor = '';
     }
 }
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
